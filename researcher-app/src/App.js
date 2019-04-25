80ba9ac5beb18d548e8cb0bd19a62851e9e0e620
David <digital0david@gmail.com>
2019-04-24 23:07:15 -0400

    reducer state

--- researcher-app/src/App.js
+++ researcher-app/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link} from "react-router-dom";
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-   
-    </div>
+    <Router>
+      <div className="App">
+    
+      </div>
+    </Router>
   );
 }
 
---
+++ researcher-app/src/reducers/index.js
@@ -0,0 +1,18 @@
+import {
+
+} from "../actions";
+
+const initialState = {
+   cards: [
+    {
+        userid: "",
+        id: 0,
+        title: "",
+        category: "",
+        description: "",
+        link: "",
+        completed: false,
+        created: new Date(),
+        updated: null
+    }
+]}
