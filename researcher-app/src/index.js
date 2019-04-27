a29728b699bb102b1527287ce9d7ffce21a19710
Kurt Van Etten <kurt@vanetten.us>
2019-04-27 12:21:11 -0400

    create Header component containing Login link

--- researcher-app/src/index.js
+++ researcher-app/src/index.js
@@ -5,17 +5,17 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
-import rootReducer from './reducers';
-
+import './reset.css';
 import './index.css';
 import App from './App';
+import rootReducer from './reducers';
+
 
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
-<Provider store={ store }>
-    <App />
-</Provider>, 
-document.getElementById('root'));
-
-
+	<Provider store={ store }>
+    	<App />
+	</Provider>, 
+	document.getElementById('root')
+);
